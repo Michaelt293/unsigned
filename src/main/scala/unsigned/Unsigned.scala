e8774ebe6cbb9935f4fd03d56b4d6f3b5d5c513c
Michael Thomas <Michaelt293@gmail.com>
2018-08-28 08:51:32 +0100

    Add Cast and Bounded typeclasses

--- src/main/scala/unsigned/Unsigned.scala
+++ src/main/scala/unsigned/Unsigned.scala
@@ -4,10 +4,15 @@ import cats._
 import cats.implicits._
 import org.scalacheck.Arbitrary
 
-import Integer._
+import Cast._
+import ToBigInt._
+import FromBigInt._
+import MaxBounded._
+import MinBounded._
 
-final class Unsigned[A: Integer] private (val value: A) {
-  def toBigInt: BigInt = value.toBigInt - Integer[A].minValue.toBigInt
+final class Unsigned[A: MinBounded : ToBigInt] private (val value: A) {
+  def toBigInt: BigInt =
+    value.toBigInt - MinBounded[A].minValue.toBigInt
 
   override def toString: String = toBigInt.toString
 
@@ -18,68 +23,46 @@ final class Unsigned[A: Integer] private (val value: A) {
 }
 
 object Unsigned {
-  private def applyWithToBigInt[A: Integer, N](n: N)(toBigInt: N => BigInt): Unsigned[A] = {
-    assert(toBigInt(n) >= BigInt(0), "Integer cannot be negative")
-    assert(toBigInt(n) <= Integer[A].maxValue.toBigInt * 2 + 1, "Integer out of range")
-    new Unsigned[A](Integer[A].fromBigInt(toBigInt(n) - Integer[A].minValue.toBigInt))
+  private def apply[A: MinBounded : ToBigInt : FromBigInt, N: ToBigInt](n: N): Unsigned[A] = {
+    assert(n.toBigInt >= BigInt(0), "Integer cannot be negative")
+    assert(n.toBigInt <= MaxBounded[A].maxValue.toBigInt * 2 + 1, "Integer out of range")
+    new Unsigned[A]((n.toBigInt - MinBounded[A].minValue.toBigInt).fromBigInt)
   }
 
-  def apply[A: Integer, N: Integer](n: N): Unsigned[A] = applyWithToBigInt(n)(_.toBigInt)
+  def apply[A: MinBounded : ToBigInt : FromBigInt](n: Byte): Unsigned[A] = apply[A, Byte](n)
 
-  def apply[A: Integer](n: Byte): Unsigned[A] = apply[A, Byte](n)
+  def apply[A: MinBounded : ToBigInt : FromBigInt](n: Short): Unsigned[A] = apply[A, Short](n)
 
-  def apply[A: Integer](n: Short): Unsigned[A] = apply[A, Short](n)
+  def apply[A: MinBounded : ToBigInt : FromBigInt](n: Int): Unsigned[A] = apply[A, Int](n)
 
-  def apply[A: Integer](n: Int): Unsigned[A] = apply[A, Int](n)
+  def apply[A: MinBounded : ToBigInt : FromBigInt](n: Long): Unsigned[A] = apply[A, Long](n)
 
-  def apply[A: Integer](n: Long): Unsigned[A] = apply[A, Long](n)
+  def apply[A: MinBounded : ToBigInt : FromBigInt](n: BigInt): Unsigned[A] = apply[A, BigInt](n)
 
-  def apply[A: Integer](n: BigInt): Unsigned[A] = applyWithToBigInt(n)(identity)
-
-  implicit def unsignedShow[A: Integer : Show]: Show[Unsigned[A]] =
+  implicit def unsignedShow[A: MinBounded : ToBigInt : FromBigInt : Show]: Show[Unsigned[A]] =
     (x: Unsigned[A]) => x.toString
 
-  implicit def unsignedEq[A: Integer : Eq]: Eq[Unsigned[A]] =
+  implicit def unsignedEq[A: MinBounded : ToBigInt : FromBigInt : Eq]: Eq[Unsigned[A]] =
     (x: Unsigned[A], y: Unsigned[A]) => x.toBigInt === y.toBigInt
 
-  implicit def unsignedOrder[A: Integer : Order]: Order[Unsigned[A]] =
+  implicit def unsignedOrder[A: MinBounded : ToBigInt : FromBigInt : Order]: Order[Unsigned[A]] =
     (x: Unsigned[A], y: Unsigned[A]) =>
         Order[BigInt].compare(x.toBigInt, y.toBigInt)
 
-  implicit def unsignedMonoid[A: Integer]: Monoid[Unsigned[A]] = {
+  implicit def unsignedMonoid[A: MinBounded : ToBigInt : FromBigInt]: Monoid[Unsigned[A]] = {
     new Monoid[Unsigned[A]] {
       def combine(x: Unsigned[A], y: Unsigned[A]): Unsigned[A] = {
-        val x_ = x.toBigInt - Integer[A].minValue.toBigInt
-        val y_ = y.toBigInt - Integer[A].minValue.toBigInt
-        val result = x.toBigInt + y.toBigInt + Integer[A].minValue.toBigInt
-        new Unsigned[A](Integer[A].fromBigInt(result))
+        val x_ = x.toBigInt - MinBounded[A].minValue.toBigInt
+        val y_ = y.toBigInt - MinBounded[A].minValue.toBigInt
+        val result = x.toBigInt + y.toBigInt + MinBounded[A].minValue.toBigInt
+        new Unsigned[A](result.fromBigInt)
       }
 
-      def empty: Unsigned[A] = new Unsigned[A](Integer[A].minValue)
+      def empty: Unsigned[A] = new Unsigned[A](MinBounded[A].minValue)
     }
   }
 
-  implicit def unsignedInteger[A: Integer]: Integer[Unsigned[A]] = {
-    new Integer[Unsigned[A]] {
-      def minValue: Unsigned[A] = Monoid[Unsigned[A]].empty
-
-      def maxValue: Unsigned[A] = new Unsigned[A](Integer[A].minValue)
-
-      def add(x: Unsigned[A], y: Unsigned[A]): Unsigned[A] = x |+| y
-
-      def subtract(x: Unsigned[A], y: Unsigned[A]): Unsigned[A] =
-        Unsigned[A](x.toBigInt - y.toBigInt)
-
-      def multiply(x: Unsigned[A], y: Unsigned[A]): Unsigned[A] =
-        Unsigned[A](x.toBigInt * y.toBigInt)
-
-      def toBigInt(x: Unsigned[A]): BigInt = x.toBigInt
-
-      def fromBigInt(x: BigInt): Unsigned[A] = apply(x)
-    }
-  }
-
-  implicit def unsignedArbitrary[A: Arbitrary : Integer]: Arbitrary[Unsigned[A]] =
+  implicit def unsignedArbitrary[A: Arbitrary]: Arbitrary[Unsigned[A]] =
     Arbitrary(
       Arbitrary.arbitrary[A].map {
         n => new Unsigned[A](n)}
--- src/test/scala/unsigned/UnsignedLaws.scala
+++ src/test/scala/unsigned/UnsignedLaws.scala
@@ -3,7 +3,7 @@ package unsigned
 import cats.kernel.laws.discipline.MonoidTests
 import cats.tests.CatsSuite
 
-class TreeLawTests extends CatsSuite {
+class UnsignedLaws extends CatsSuite {
   import Unsigned._
   checkAll("Unsigned[Byte].MonoidTests", MonoidTests[Unsigned[Byte]].monoid)
   checkAll("Unsigned[Short].MonoidTests", MonoidTests[Unsigned[Short]].monoid)
